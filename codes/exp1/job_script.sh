15b877e4f32d240d520b0eec39e7d88319084270
ckorkmaz14 <ckorkmaz14@login02.kuacc.ku.edu.tr>
2023-01-02 00:25:54 +0300

    bug fixed, ready for training

---
+++ codes/exp1/job_script.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# 
+# CompecTA (c) 2017
+#
+# You should only work under the /scratch/users/<username> directory.
+#
+# Example job submission script
+#
+# TODO:
+#   - Set name of the job below changing "Test" value.
+#   - Set the requested number of tasks (cpu cores) with --ntasks parameter.
+#   - Select the partition (queue) you want to run the job in:
+#     - short : For jobs that have maximum run time of 120 mins. Has higher priority.
+#     - long  : For jobs that have maximum run time of 7 days. Lower priority than short.
+#     - longer: For testing purposes, queue has 31 days limit but only 3 nodes.
+#   - Set the required time limit for the job with --time parameter.
+#     - Acceptable time formats include "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds"
+#   - Put this script and all the input file under the same directory.
+#   - Set the required parameters, input and output file names below.
+#   - If you do not want mail please remove the line that has --mail-type
+#   - Put this script and all the input file under the same directory.
+#   - Submit this file using:
+#      sbatch examle_submit.sh
+
+# -= Resources =-
+#
+#SBATCH --job-name=denoiser
+#SBATCH --nodes=1
+#SBATCH --ntasks=5
+#SBATCH --partition=ai
+#SBATCH --qos=ai
+#SBATCH --account=ai
+##SBATCH --exclude=da[01-04],ag01,sm01,ai07,ai10,ai11,be[01-12],ai05,ai02,ai06,ai04
+#SBATCH --constraint=tesla_k80
+##SBATCH --nodelist=ai16
+#SBATCH --mem=50GB
+#SBATCH --gres=gpu:1
+#SBATCH --time=7-0
+#SBATCH --output=sonuc-%j.out
+#SBATCH --error=sonuc-%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ckorkmaz14@ku.edu.tr
+
+################################################################################
+##################### !!! DO NOT EDIT BELOW THIS LINE !!! ######################
+################################################################################
+
+echo "Setting stack size to unlimited..."
+ulimit -s unlimited
+ulimit -l unlimited
+ulimit -a
+echo
+
+echo "==============================================================================="
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/kuacc/users/ckorkmaz14/miniconda3/lib/
+source /kuacc/users/ckorkmaz14/py_venvs/cansu/bin/activate
+nvidia-smi
+python train.py
---
+++ codes/exp1/misc.py
@@ -0,0 +1,84 @@
+import random, numpy, torch, os, imageio
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+def seed_torch(seed=1234):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    numpy.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def save_model(model, optimizer, saving_path):
+    state = {
+        "state_dict": model.state_dict(),
+        "optimizer": optimizer.state_dict()
+    }
+    torch.save(state,saving_path)
+
+def load_model(model, optimizer, saving_path):
+    checkpoint = torch.load(saving_path)
+    model.load_state_dict(checkpoint["state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+    return model, optimizer
+
+def parameters_count(model):
+    total = sum([i.numel() for i in model.parameters()])
+    trainable = sum([i.numel() for i in model.parameters() if i.requires_grad])
+    print("Total parameters : {}. Trainable parameters : {}".format(total, trainable))
+    return total, trainable
+
+def evaluation(model, valid_loader, l1_loss, tv_loss, device):
+    test_loss = 0
+    model.eval()
+    with torch.no_grad():
+        for i, (input_img, output_img) in enumerate(tqdm(valid_loader)):
+
+            input_img       = input_img.to(device).float()                   
+            output_img      = output_img.to(device).float()
+            mask_prediction, img_prediction = model(input_img)
+
+            loss_tv_mask  = tv_loss(mask_prediction)
+            loss_l1       = l1_loss(img_prediction, output_img)
+            loss          = loss_tv_mask + loss_l1
+            test_loss       += loss
+
+
+        test_loss /= len(valid_loader)
+    return test_loss
+
+def evaluation_with_img_saving(model, valid_loader, l1_loss, tv_loss, result_path, dataset_name, img_names, device):
+    model.eval()
+    with torch.no_grad():
+        for i, (input_img, output_img) in enumerate(tqdm(valid_loader)):
+
+            input_img       = input_img.to(device).float()                   
+            output_img      = output_img.to(device).float()
+            mask_prediction, img_prediction = model(input_img)
+
+            img_prediction     = img_prediction[0].permute(1,2,0).detach().cpu().numpy()
+            img_prediction_out = numpy.round((img_prediction*255)).clip(0,255).astype('uint8')
+
+            img_name   = (img_names[i].split('/')[-1]).split(".")[0] 
+            imageio.imsave(result_path + dataset_name + '/' + img_name +'_UDR.png', img_prediction_out)
+
+
+# https://discuss.pytorch.org/t/check-gradient-flow-in-network/15063/7
+def plot_grad_flow(named_parameters):
+    ave_grads = []
+    layers = []
+    for n, p in named_parameters:
+        if(p.requires_grad) and ("bias" not in n):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean())
+    plt.plot(ave_grads, alpha=0.3, color="b")
+    plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
+    plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(xmin=0, xmax=len(ave_grads))
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.imsave("a.png")
